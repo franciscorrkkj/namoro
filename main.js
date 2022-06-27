02a31ddd4bd63ea6603c870e928a1d12e945700b
francicorrr <108167052+franciscorrkkj@users.noreply.github.com>
2022-06-26 23:08:30 -0400

    namorecomigo

---
+++ main.js
@@ -0,0 +1,103 @@
+function load() {
+    window.location.href = "pag-1.html"
+}
+
+function load1() {
+    window.location.href = "pag-2.html"
+}
+
+function load2() {
+    window.location.href = "pag-3.html"
+}
+
+function load3() {
+    window.location.href = "pag-4.html"
+}
+
+function load4() {
+    window.location.href = "pag-5.html"
+}
+
+function load5() {
+    window.location.href = "pag-6.html"
+}
+
+function load6() {
+    window.location.href = "pag-7.html"
+}
+
+function load7() {
+    window.location.href = "pag-8.html"
+}
+
+function load8() {
+    window.location.href = "pag-9.html"
+}
+
+function load9() {
+    window.location.href = "pag-10.html"
+}
+
+function load10() {
+    window.location.href = "pag-11.html"
+}
+
+function voltar() {
+    window.location.href = "index.html"
+}
+
+function voltar1() {
+    window.location.href = "pag-1.html"
+}
+
+function voltar2() {
+    window.location.href = "pag-2.html"
+}
+
+function voltar3() {
+    window.location.href = "pag-3.html"
+}
+
+function voltar4() {
+    window.location.href = "pag-4.html"
+}
+
+function voltar5() {
+    window.location.href = "pag-5.hmtl"
+}
+
+function voltar6() {
+    window.location.href = "pag-6.html"
+}
+
+function voltar7() {
+    window.location.href = "pag-7.html"
+}
+
+function voltar8() {
+    window.location.href = "pag-8.html"
+}
+
+function voltar9() {
+    window.location.href = "pag-9.html"
+}
+
+function voltar10() {
+    window.location.href = "pag-9.html"
+}
+
+function load11() {
+    window.location.href = "pag-12.html"
+}
+
+function voltarpedido() {
+    window.location.href = "pag-12.html"
+}
+
+function sim() {
+    window.location.href = "pag-sim.html"
+}
+
+function nao() {
+    window.location.href = "pag-nao.html"
+}
